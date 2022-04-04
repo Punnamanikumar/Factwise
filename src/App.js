629950999c0025bfe6b18e94c849834853dd0d5d
Punnamanikumar <punnamanikumar@gmail.com>
2022-04-04 14:47:40 +0530

    factwise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import AllComponents from "./Components/AllComponents";
-import Example from "./Components/Example";
 
 function App() {
   return (
     <div className="App">
       <AllComponents />
-      {/* <Example /> */}
     </div>
   );
 }

7ff84eb4604b52dda44a85e0d58a1e1abbb573e8
Punnamanikumar <punnamanikumar@gmail.com>
2022-04-04 14:44:56 +0530

    factwise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllComponents from "./Components/AllComponents";
+import Example from "./Components/Example";
+
+function App() {
+  return (
+    <div className="App">
+      <AllComponents />
+      {/* <Example /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Example.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { EditText, EditTextarea } from "react-edit-text";
+// import "react-edit-text/dist/index.css";
+
+export default function Example() {
+  return (
+    <div>
+      <EditText showEditButton />
+      <EditTextarea />
+    </div>
+  );
+}

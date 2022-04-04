cf4aba80e017c9d1e2ac26cf0a07780cd163307c
Punnamanikumar <punnamanikumar@gmail.com>
2022-04-04 18:56:57 +0530

    factwise

--- src/Components/AllComponents.js
+++ src/Components/AllComponents.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Celebrities from "./celebrities.json";
 import "./Background.css";
 import { EditTextarea } from "react-edit-text";
